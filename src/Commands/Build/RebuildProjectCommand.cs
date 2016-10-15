1da7edb48e2f892844c393a1d3805a44abca276c
Yann Duran <yd@live.com.au>
2016-10-15 23:32:19 +1100

    Add ignored files

---
+++ src/Commands/Build/RebuildProjectCommand.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.Shell;
+
+namespace TimeSavers.VS.Commands.Build
+{
+    using YD.Framework.VisualStudio.Commands;
+    using YD.Framework.VisualStudio.Packages;
+
+    internal sealed class RebuildProjectCommand : BuildCommand
+    {
+        //***
+        //===M
+
+        private RebuildProjectCommand(PackageBase package)
+            : base(package, PackageIds.RebuildProjectCommand)
+        { }
+
+        //===M
+
+        public static void Instantiate(PackageBase package)
+            => Instantiate(new RebuildProjectCommand(package));
+
+        //---
+
+        protected override bool CanExecute
+        => (base.CanExecute && BuildOptions.RebuildProjectCommandEnabled);
+
+        protected override bool IsActive
+            => base.IsActive && SolutionHasProjects && SolutionIsNotBuilding;// && ProjectSelected();
+
+        protected override void OnExecute(OleMenuCommand command)
+            => ExecuteCommand()
+                .ShowProblem()
+                .ShowInformation();
+
+        //---
+
+        private CommandResult ExecuteCommand()
+            => Package?.BuildProject(rebuild: true);
+
+        //***
+    }
+}
---
+++ src/Commands/Build/RebuildSolutionCommand.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.Shell;
+
+namespace TimeSavers.VS.Commands.Build
+{
+    using YD.Framework.VisualStudio.Commands;
+    using YD.Framework.VisualStudio.Packages;
+
+    internal sealed class RebuildSolutionCommand : BuildCommand
+    {
+        //***
+        //===M
+
+        private RebuildSolutionCommand(PackageBase package) : base(package, PackageIds.RebuildSolutionCommand)
+        { }
+
+        //===M
+
+        public static void Instantiate(PackageBase package)
+            => Instantiate(new RebuildSolutionCommand(package));
+
+        //---
+
+        protected override bool CanExecute
+        => (base.CanExecute && BuildOptions.RebuildSolutionCommandEnabled);
+
+        protected override bool IsActive
+            => base.IsActive && SolutionHasProjects && SolutionIsNotBuilding;
+
+        protected override void OnExecute(OleMenuCommand command)
+            => ExecuteCommand()
+                .ShowProblem()
+                .ShowInformation();
+
+        //---
+
+        private CommandResult ExecuteCommand()
+            => Package?.BuildSolution(rebuild: true);
+
+        //***
+    }
+}
