889dc5ca159c7043deb8d889310ac37f55797c93
Yann Duran <yd@live.com.au>
2016-10-16 03:14:13 +1100

    Fix manifest details
    - description
    - tags

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -9,7 +9,7 @@ namespace TimeSavers.VS
     {
         public const string Id = "049c7ac5-ba44-4a72-b4ee-7be7fb1b0edd";
         public const string Name = "Visual Studio Time Savers";
-        public const string Description = @"A collection of essential productivity enhancements for day-to-day developing in Visual Studio 2015.";
+        public const string Description = @"A growing collection of small time-savers to enhance day-to-day productivity for developing with Visual Studio.";
         public const string Language = "en-US";
         public const string Version = "1.0.0";
         public const string Author = "Yann Duran";
